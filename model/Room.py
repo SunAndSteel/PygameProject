36506093786e15b6b548a2c61b8ee717c132ebf0
Florent Urbain <florenturbain@protonmail.com>
2024-05-13 15:23:10 +0200

    fix: code en trop

--- model/Room.py
+++ model/Room.py
@@ -11,23 +11,6 @@ class Room:
             [1, 1, 1, 1, 1]
         ]
         self.tile_colors = {
-            0: (0, 255, 255),  # White
-            1: (0, 0, 0) # Black
-        }
-
-
-class Room:
-    def __init__(self, number):
-        self._number = number
-        self._tile_size = 32
-        self._tile_map = [
-            [1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 1, 1, 1, 1]
-        ]
-        self.tile_colors = {
             0: (0, 0, 0),  # White
             1: (255, 255, 255) # Black
         }
