3ed7b4bf19cf1aa5b342d72d284e05fd466f041a
El_Pablo <robin.gillard1@std.heh.be>
2024-05-13 15:19:05 +0200

    Menu adding the difficulty option

--- Groupe 19/Binding Of Isaac/Menu.py
+++ Groupe 19/Binding Of Isaac/Menu.py
@@ -8,6 +8,7 @@ pygame.display.set_caption("Menu")
 
 BG = pygame.image.load("assets/Graphics/Background_menu.jpg")
 
+difficulty = 'NORMAL'
 
 def get_font(size):  # Returns Press-Start-2P in the desired size
     return pygame.font.Font("assets/font/The walking font.ttf", size)
@@ -41,21 +42,34 @@ def play():
 
 
 def options():
+    global difficulty
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
         SCREEN.fill("white")
 
         OPTIONS_TEXT = get_font(45).render("Chose your game difficulty", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 100))
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-        OPTIONS
-        OPTIONS_BACK = Button(image=None, pos=(640, 460),
-                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+        OPTIONS_EASY = Button(image=None, pos=(200, 360),
+                             text_input="EASY", font=get_font(75), base_color="Black",hovering_color='Grey', selected=difficulty=='EASY')
+        OPTIONS_NORMAL = Button(image=None, pos=(640, 360),
+                               text_input="NORMAL", font=get_font(75), base_color="Black",hovering_color='Grey', selected=difficulty=='NORMAL')
+        OPTIONS_HARD = Button(image=None, pos=(1080, 360),
+                             text_input="HARD", font=get_font(75), base_color="Black",hovering_color="Grey", selected=difficulty=='HARD')
+        OPTIONS_BACK = Button(image=None, pos=(640, 650),
+                              text_input="GO BACK", font=get_font(75), base_color="Black", hovering_color="Grey")
 
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(SCREEN)
+        # Update base_color based on selected attribute
+        OPTIONS_EASY.base_color = "Grey" if OPTIONS_EASY.selected else "Black"
+        OPTIONS_NORMAL.base_color = "Grey" if OPTIONS_NORMAL.selected else "Black"
+        OPTIONS_HARD.base_color = "Grey" if OPTIONS_HARD.selected else "Black"
+
+        mouse_pos = pygame.mouse.get_pos()
+        for button in [OPTIONS_BACK, OPTIONS_EASY, OPTIONS_NORMAL, OPTIONS_HARD]:
+            button.changeColor(mouse_pos)  # Changer la couleur du bouton si la souris est dessus
+            button.update(SCREEN)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -64,10 +78,23 @@ def options():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
                     main_menu()
+                if OPTIONS_EASY.checkForInput(OPTIONS_MOUSE_POS):
+                    difficulty = 'EASY'
+                    OPTIONS_EASY.selected = True
+                    OPTIONS_NORMAL.selected = False
+                    OPTIONS_HARD.selected = False
+                if OPTIONS_NORMAL.checkForInput(OPTIONS_MOUSE_POS):
+                    difficulty = 'NORMAL'
+                    OPTIONS_EASY.selected = False
+                    OPTIONS_NORMAL.selected = True
+                    OPTIONS_HARD.selected = False
+                if OPTIONS_HARD.checkForInput(OPTIONS_MOUSE_POS):
+                    difficulty = 'HARD'
+                    OPTIONS_EASY.selected = False
+                    OPTIONS_NORMAL.selected = False
+                    OPTIONS_HARD.selected = True
 
         pygame.display.update()
-
-
 def main_menu():
     while True:
         SCREEN.blit(BG, (0, 0))
@@ -80,7 +107,7 @@ def main_menu():
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Graphics/Play Rect.png"), pos=(640, 250),
                              text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
         OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Graphics/Options Rect.png"), pos=(640, 400),
-                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+                                text_input="DIFFICULTY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
         QUIT_BUTTON = Button(image=pygame.image.load("assets/Graphics/Quit Rect.png"), pos=(640, 550),
                              text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
