eaf1762a6c608c996013f36264241c8c458d2e86
El_Pablo <robin.gillard1@std.heh.be>
2024-05-15 18:23:46 +0200

    File lost, re pushing to the branch the local copy

---
+++ Groupe 19/Binding Of Isaac/Mob.py
@@ -0,0 +1,118 @@
+import json
+import pygame
+from Boss import Boss
+from math import sqrt
+from Hero import *
+from Projectile import Projectile
+
+
+HAUTEUR, LARGEUR = 800, 800
+
+# Initialisation de Pygame
+pygame.init()
+screen = pygame.display.set_mode((HAUTEUR, LARGEUR))
+clock = pygame.time.Clock()
+
+
+class Mob(Boss):
+    def __init__(self, image, x, y, target,Id=2 ,path='Entitys/Mobs/Normal_Mobs/RandomMob.json'):
+        super().__init__(image, x, y, target, path)
+        self.health = 100
+        self.deplacement = "right-left"
+        self.level = 50
+        self.Id = Id
+        self.load_mob_data(path)
+        self.target = target  # Le joueur que le mob doit poursuivre
+        self.attack_range = 30  # Define the attack range of the mob
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.last_attack_time = pygame.time.get_ticks()  # Store the time of the last attack
+
+    def load_mob_data(self, path):
+        try:
+            with open(path, 'r') as file:
+                mob_data = json.load(file)
+                self.health = mob_data.get("health", self.health)
+                self.deplacement = mob_data.get("deplacement", self.deplacement)
+                self.level = mob_data.get("level", self.level)
+        except FileNotFoundError:
+            print(f"Erreur : fichier JSON introuvable - {path}")
+        except json.JSONDecodeError:
+            print(f"Erreur : fichier JSON malformé - {path}")
+        except Exception as e:
+            print(f"Erreur lors du chargement des données depuis le fichier JSON: {e}")
+
+    def hurt(self, damage, mobs):
+        self.health -= damage
+        print(f"Mob health: {self.health}")
+        if self.health <= 0:
+            self.kill(mobs)  # Remove the mob if its health reaches 0
+
+    def move(self):
+        if self.Id == 1:
+            dx, dy = self.target.rect.x - self.rect.x, self.target.rect.y - self.rect.y
+            dist = sqrt(dx ** 2 + dy ** 2)
+
+            # Normalize the direction vector (dx, dy) -> (dx/dist, dy/dist)
+            if dist > 0:
+                dx, dy = dx / dist, dy / dist
+
+            # Update the boss's position
+            self.rect.x += dx * self.speed
+            self.rect.y += dy * self.speed
+
+    def shoot(self):
+        from main import projectiles
+        if self.Id == 2:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.last_attack_time >= 1500:
+                # Create a new projectile
+                projectile = Projectile(self.rect.center, self.target.rect.center)
+                # Add the projectile to the projectiles group
+                projectiles.add(projectile)
+            self.last_attack_time = current_time
+
+
+    def kill(self, mobs):
+        if self in mobs:
+            mobs.remove(self)
+        super().kill()  # Call the kill method of the superclass
+
+    def update(self):
+        self.move()
+        self.shoot()
+        super().update()
+
+    def attack(self, target, projectiles):
+        current_time = pygame.time.get_ticks()
+        if self.Id == 2:
+            if current_time - self.last_attack_time >= 1500:  # Check if 1.5 seconds have passed since the last attack
+                # Create a new projectile
+                projectile = Projectile(self.rect.center, target.rect.center)  # Pass the target object itself
+                # Add the projectile to the projectiles grou
+                print("ZAAAAAAAAAAAAAAAAAAAAAAAA")
+                projectiles.add(projectile)
+            self.last_attack_time = current_time
+
+
+    def intersects(self, other):
+        return self.rect.colliderect(other.rect)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+    def show_informations(self):
+        super().show_informations()
+        if self.show_player_information:
+            info_surface = pygame.Surface((200, 50))
+            info_surface.fill((0, 0, 0))
+            info = [
+                f"Résistance: {self.resistance}",
+                f"Deplacement: {self.deplacement}",
+                f"Level: {self.level}"
+            ]
+            for i, text in enumerate(info):
+                info_surface.blit(pygame.font.SysFont(None, 20).render(text, True, (255, 255, 255)), (10, i * 20))
+            screen.blit(info_surface, (self.rect.x, self.rect.y - 50))
+            pygame.display.flip()
+
--- Groupe 19/Binding Of Isaac/main.py
+++ Groupe 19/Binding Of Isaac/main.py
@@ -1,15 +1,182 @@
 import pygame
 from sys import exit
+from Menu_button import Button
+import time
+from Hero import Hero
+from Mob_spawn import add_mob, mobs, obstacles, add_boss, add_obstacle
+from Weapons import *
+from Obstacle import *
+from Boss import *
+from RoomV2 import *
+
 
 pygame.init()
+music = pygame.mixer.music.load("assets/Sound/game track.mp3")
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.3)
+width, height = 1280, 720
+
+screen = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption("Binding of Isaac")
 
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+fps = pygame.time.Clock()
+pause = False
+font = pygame.font.Font('assets/font/Hammer God Font DEMO.ttf', 36)
+projectiles = pygame.sprite.Group()
 
 
-    pygame.display.update()
+
+# def pause_handling(screen, font):
+#     global pause
+#
+#     MOUSE_POS = pygame.mouse.get_pos()
+#     # Create the text
+#     text = font.render("Game paused : press Escape to resume", True, 'black')
+#     text_rect = text.get_rect(center=(width/2, height/2 - 100))
+#     screen.blit(text, text_rect)
+#
+#     # Create the buttons
+#     resume_button = Button(image=None, pos=(width/2, height/2), text_input="Resume", font=font, base_color="Black", hovering_color="Green")
+#     save_button = Button(image=None, pos=(width/2, height/2 + 100), text_input="Save", font=font, base_color="Black", hovering_color="Green")
+#     quit_button = Button(image=None, pos=(width/2, height/2 + 200), text_input="Quit", font=font, base_color="Black", hovering_color="Green")
+#
+#     # Draw the buttons
+#     mouse_pos = pygame.mouse.get_pos()
+#     for button in [resume_button, save_button, quit_button]:
+#         button.changeColor(mouse_pos)
+#         button.update(screen)
+#
+#     # Check for mouse events
+#     for event in pygame.event.get():
+#         if event.type == pygame.MOUSEBUTTONDOWN:
+#             if resume_button.checkForInput(MOUSE_POS):
+#                 pause = not pause
+#                 print("Resume game 1")
+#             if save_button.checkForInput(MOUSE_POS):
+#                 print("Save game functionality to be implemented")
+#             if quit_button.checkForInput(MOUSE_POS):
+#                 pygame.quit()
+#
+#     pygame.display.update()
+
+
+
+# Lire les données JSON
+with open('data.json', 'r') as f:
+    data = json.load(f)
+
+# Créer l'objet MapOfRoom avec le nombre de pièces souhaité
+map_of_room = MapOfRoom(data["map_dir"], num_rooms=4)
+
+hero = Hero("Entitys/Mobs/Hero/hero.json")
+all_sprites = pygame.sprite.Group()
+all_sprites.add(hero)
+
+def game(screen, font, pause=False):
+    original_screen = screen.copy()  # Make a copy of the original screen
+    pause_bg = None
+    screenshot_taken = False
+    boss_spawned = False
+    if hero.in_boss_room:
+        # If the player is in the boss room, spawn the boss and obstacles
+        if not boss_spawned and len(mobs) == 0:
+            print("hereez")
+            add_boss(hero)  # You need to implement this function
+            boss_spawned = True
+    else:
+        # If the player is not in the boss room, spawn the mobs
+        while len(mobs) < 5:
+            add_mob(hero)
+    while len(obstacles) < 2:
+        print("Adding obstacle")
+        add_obstacle(hero)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                pass
+            # Add a mob when the timer event occurs
+
+        #         if event.key == pygame.K_ESCAPE:
+        #             pause = not pause
+        #             if pause:
+        #                 if not screenshot_taken:
+        #                     pygame.image.save(screen, "pause.png")
+        #                     screenshot_taken = True
+        #                     pause_bg = pygame.image.load("pause.png").convert_alpha()
+        #                     pause_bg_gs = pygame.Surface(pause_bg.get_size())
+        #                     pygame.transform.grayscale(pause_bg, pause_bg_gs)
+        #                     pygame.mixer.music.pause()
+        #                 screen.blit(pause_bg_gs, (0, 0))
+        #             else:
+        #                 pygame.mixer.music.unpause()
+        #                 screenshot_taken = False
+        #
+        # # Blit other elements onto the screen here
+        #
+        # if pause:
+        #     pause_handling(screen, font)  # Draw pause menu
+        # else:
+        #     # Restore the original screen when unpaused
+        #     screen.blit(original_screen, (0, 0))
+
+        if pygame.sprite.spritecollide(hero, doors_group, False):
+            print("Player collided with a door!")
+
+
+        # Update and draw the projectiles
+
+        map_of_room.draw(screen)
+
+        hero.attack(mobs)
+        hero.update(mobs, obstacles)
+
+        for mob in mobs:
+            mob.attack(hero, projectiles)  # Add this line
+            mob.update()
+            mob.draw(screen)
+
+            if isinstance(mob, Boss):
+                mob.attack(hero, projectiles)  # Add this line
+
+        projectiles.update()
+
+        for projectile in projectiles:
+            if hero.rect.colliderect(projectile.rect):
+                hero.hurt(20)  # Call the hurt method of the player
+                projectile.kill()  # Remove the projectile
+
+        for sprite in all_sprites:
+            if sprite != hero:
+                sprite.update()
+        all_sprites.draw(screen)
+
+        # Draw all the mobs
+        for mob in mobs:
+            mob.draw(screen)
+
+        for obstacle in obstacles:
+            obstacle.draw(screen)
+
+        for projectiled in projectiles:
+            projectiled.draw(screen)
+
+        if isinstance(hero.weapon, Gun):
+            hero.weapon.update(screen, mobs)
+
+        hero.draw(screen)
+
+
+
+        
+        pygame.display.update()
+
+        fps.tick(120)
+
+# Start the game
+game(screen, font, pause=False)
